9dd8598b5960d8579a7e62e7b44fd5311ab56e22
Denys Stashchenko <derdek02@gmail.com>
2024-12-04 00:37:50 +0200

    change last_hash to version

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/version', function () {
-    return response()->json(['last_hash' => config('app.version')]);
+    return response()->json(['version' => config('app.version')]);
 });
