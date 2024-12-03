4a9eb5de1c0b495ee53477a2fb736d96212874e1
Denys Stashchenko <derdek02@gmail.com>
2024-12-04 00:57:12 +0200

    add task crud

--- laravel-project/routes/api.php
+++ laravel-project/routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/version', function () {
     return response()->json(['version' => config('app.version')]);
 });
+
+Route::get('/tasks', [TaskController::class, 'getTasks']);
+Route::post('/tasks', [TaskController::class, 'createTask']);
+Route::get('/tasks/{task}', [TaskController::class, 'getTask']);
+Route::put('/tasks/{task}', [TaskController::class, 'updateTask']);
+Route::delete('/tasks/{task}', [TaskController::class, 'deleteTask']);
