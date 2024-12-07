3e46ab119a33452e3728070d4fdb7571ad0cc797
Denys Stashchenko <derdek02@gmail.com>
2024-12-08 01:13:39 +0200

    remove auth

--- laravel-project/config/log-viewer.php
+++ laravel-project/config/log-viewer.php
@@ -4,10 +4,10 @@ return [
         'production' => [
             'name' => 'Production',
             'host' => 'http://192.168.122.215/log-viewer',
-            'auth' => [
-                'username' => env('LOG_VIEWER_PRODUCTION_USERNAME', 'admin'),
-                'password' => env('LOG_VIEWER_PRODUCTION_PASSWORD', 'ChangeMe'),
-            ],
+//            'auth' => [
+//                'username' => env('LOG_VIEWER_PRODUCTION_USERNAME', 'admin'),
+//                'password' => env('LOG_VIEWER_PRODUCTION_PASSWORD', 'ChangeMe'),
+//            ],
         ],
     ],
 ];
